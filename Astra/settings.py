f2482fbe8cf1fcc16777e2f46c90e20bf04df310
savysanish56 <86150580+savysanish56@users.noreply.github.com>
2022-10-06 02:43:55 +1030

    updates and changes

--- Astra/settings.py
+++ Astra/settings.py
@@ -181,7 +181,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = 'True'
 EMAIL_HOST_USER = 'troom8296@gmail.com'
-EMAIL_HOST_PASSWORD = 'equcavzibecdsqep'
+EMAIL_HOST_PASSWORD = 'yjowztctpskcubrg'
 
 
 
--- Hacker/urls.py
+++ Hacker/urls.py
@@ -38,7 +38,7 @@ urlpatterns = [
     path('delete-message/<str:pk>/', views.deleteMessage, name = "delete-message"),
 
     path('update-user', views.updateUser, name = "update-user"),
-    path('login/', views.loginCustomer, name = 'login'),
+    # path('login/', views.loginCustomer, name = 'login'),
     # path('register-customer/', views.registerCustomer, name = 'register-customer'),
     ## for flexible device design 
     path('topics/', views.topicsPage, name = 'topics'),
