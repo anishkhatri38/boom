95300cff9f599eaec9e8f07dd9a95981482aa625
savysanish56 <86150580+savysanish56@users.noreply.github.com>
2022-10-06 13:08:29 +1030

    updates

--- Hacker/models.py
+++ Hacker/models.py
@@ -136,25 +136,8 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
-#for payment configuration using stripe
 
 
-class FitnessPlan(models.Model):
-    name = models.CharField(max_length=255)
-    price = models.IntegerField(default = 0) #cents
-
-    def __str__(self):
-        return self.name
-
-# class HomeContact(models.Model):
-#     name=models.CharField (max_length=122)
-#     email=models.CharField (max_length=122)
-#     phone=models.CharField (max_length=12)
-#     desc=models.TextField()
-#     date=models.DateField()
-
-#     def __str__(self):
-#         return self.name
 
 
 class Blog(models.Model):
@@ -162,10 +145,6 @@ class Blog(models.Model):
     content = models.TextField()
     slug = models.SlugField(max_length = 255)
     featured_image = models.ImageField(upload_to= "img/Blog")
-    
-    def get_absolute_url(self):
-        return reverse("Astra:single", args=[self.slug])
-
 
     def __str__(self):
         return self.title
